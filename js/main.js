f34ce3495badd38b4ab95fc8a70536416f42f4ec
Kevin C McKenna <kevmckenna@gmail.com>
2017-08-28 10:06:21 -0700

    set up button listener

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function sayHello(name) {
+    return "Hello " + (name || "anonymous person")
+}
+
+function setH1(string) {
+    var h1 = document.querySelector('h1')
+    h1.innerText = string
+}
+
+function showGreeting() {
+    setH1(sayHello("Kevin"))
+}
+
+var button = document.querySelector('button')
+button.addEventListener('click', showGreeting)
+
+//showGreeting is a "callback", something that accounts for the future action//
+
+button.addEventListener('click', function(){
+    setH1(seyHello("BANANAS"))
+})
+
+//the function here is called "anonymous", used when only wanting to use a particular example by declaring that function//
